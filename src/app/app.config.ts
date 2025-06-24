af9f0f350bd17c0e8f789dbb27289fea60dadd8d
RadoslawNagiel <nagiel.media@gmail.com>
2025-06-24 20:02:33 +0200

    Feat: Initialize translations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,20 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { environment } from './environments/environment';
+import { TranslationService } from './translations/translation.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideTranslateService({
+      defaultLanguage: environment.defaultLanguage,
+      loader: {
+        provide: TranslateLoader,
+        useClass: TranslationService,
+      },
+    }),
   ],
 };
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  defaultLanguage: `pl`,
+  availableLanguages: [
+    {
+      langKey: `pl`,
+      value: `pl`,
+    },
+    {
+      langKey: `en`,
+      value: `en`,
+    },
+  ],
+};
