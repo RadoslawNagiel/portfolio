5d39511866ca1888d2d46b4eb61812e823f46142
RadoslawNagiel <nagiel.media@gmail.com>
2025-06-29 21:36:23 +0200

    Feat: Add safety Hab project

--- src/app/shared/data/tags.ts
+++ src/app/shared/data/tags.ts
@@ -1,18 +1,46 @@
 export enum TAG {
+  typescript = 'typescript',
+
+  angular16 = 'angular16',
   angular17 = 'angular17',
   angular18 = 'angular18',
   angular19 = 'angular19',
-  typescript = 'typescript',
+
+  primeNG = 'primeNG',
   ionic = 'ionic',
   capacitor = 'capacitor',
+
+  apollo = 'apollo',
+  nestJS = 'nestJS',
+  graphQl = 'graphQl',
+  prisma = 'prisma',
+  keycloak = 'keycloak',
+
   ngxs = 'ngxs',
-  chartJs = 'chartJs',
   firebase = 'firebase',
-  eslint = 'eslint',
+  sentry = 'sentry',
+
+  chartJs = 'chartJs',
+  echarts = 'echarts',
+
+  luxon = 'luxon',
+  ngxTranslate = 'ngxTranslate',
   tailwindcss = 'tailwindcss',
+  eslint = 'eslint',
 }
 
 export const TAGS = {
+  typescript: {
+    name: `TypeScript`,
+    background: `#3178C6`,
+    color: `white`,
+  },
+
+  angular16: {
+    name: `Angular 16`,
+    background: `#dd0031`,
+    color: `white`,
+  },
   angular17: {
     name: `Angular 17`,
     background: `#dd0031`,
@@ -28,9 +56,10 @@ export const TAGS = {
     background: `#dd0031`,
     color: `white`,
   },
-  typescript: {
-    name: `TypeScript`,
-    background: `#3178C6`,
+
+  primeNG: {
+    name: `PrimeNG`,
+    background: `#C3002F`,
     color: `white`,
   },
   ionic: {
@@ -43,14 +72,36 @@ export const TAGS = {
     background: `#53B9FF`,
     color: `white`,
   },
+
+  apollo: {
+    name: `Apollo`,
+    background: `#14252D`,
+    color: `white`,
+  },
+  nestJS: {
+    name: `NestJS`,
+    background: `#E0224E`,
+    color: `white`,
+  },
+  graphQl: {
+    name: `GraphQl`,
+    background: `#E10098`,
+    color: `white`,
+  },
+  prisma: {
+    name: `Prisma`,
+    background: `#0C344B`,
+    color: `white`,
+  },
+  keycloak: {
+    name: `Keycloak`,
+    background: `#008AAA`,
+    color: `white`,
+  },
+
   ngxs: {
     name: `NGxS`,
     background: `#1E00FF`,
-    color: `white`,
-  },
-  chartJs: {
-    name: `chartJs`,
-    background: `#FE777B`,
     color: `white`,
   },
   firebase: {
@@ -58,9 +109,36 @@ export const TAGS = {
     background: `#FFCA2D`,
     color: `white`,
   },
+  sentry: {
+    name: `Sentry`,
+    background: `#864F90`,
+    color: `white`,
+  },
+
+  chartJs: {
+    name: `chartJs`,
+    background: `#FE777B`,
+    color: `white`,
+  },
+  echarts: {
+    name: `ECharts`,
+    background: `#A2344A`,
+    color: `white`,
+  },
+
+  luxon: {
+    name: `Luxon`,
+    background: `#B792FF`,
+    color: `white`,
+  },
   eslint: {
     name: `ESLint`,
     background: `#4B32C3`,
+    color: `white`,
+  },
+  ngxTranslate: {
+    name: `ngx-translate`,
+    background: `#373B4D`,
     color: `white`,
   },
   tailwindcss: {
