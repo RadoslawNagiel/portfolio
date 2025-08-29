5176a4af8671579eccd2e3a48837b50029c115ba
RadoslawNagiel <nagiel.media@gmail.com>
2025-08-29 16:58:26 +0200

    Chore: Update filters components

--- src/app/shared/dumb-components/filter-label/filter-label.component.ts
+++ src/app/shared/dumb-components/filter-label/filter-label.component.ts
@@ -1,15 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component, input, output } from '@angular/core';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
+import { IconButtonComponent } from '../icon-button/icon-button.component';
 
 @Component({
   selector: 'app-filter-label',
-  imports: [TranslateModule, MatTooltipModule],
+  imports: [TranslateModule, NgClass, IconButtonComponent],
   templateUrl: './filter-label.component.html',
   styleUrl: './filter-label.styles.scss',
 })
 export class FilterLabelComponent {
   label = input.required<string>();
+  buttonTooltip = input(`filters.clearTooltip`);
+  labelClass = input<string>();
   displayClearIcon = input(false);
   clear = output();
 }
