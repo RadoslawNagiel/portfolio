185b4f505dee0fbb237cc11f133948f10253747a
RadoslawNagiel <nagiel.media@gmail.com>
2025-08-30 15:26:12 +0200

    Fix: Menu bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
@@ -17,4 +17,14 @@ import { MenuItemsComponent } from './shared/smart-components/menu-items/menu-it
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  menuOpened = signal(false);
+
+  toggleMenu() {
+    this.menuOpened.set(!this.menuOpened());
+    const body = document.getElementById(`body-element`);
+    if (body) {
+      body.style.overflow = this.menuOpened() ? `hidden` : `auto`;
+    }
+  }
+}
