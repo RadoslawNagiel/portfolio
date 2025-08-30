f1ae460d261f02ec40e7afd549fd3e992b791bf4
RadoslawNagiel <nagiel.media@gmail.com>
2025-08-30 18:04:58 +0200

    Feat: Improvement mobile UX

--- src/app/shared/dumb-components/top-button/top-button.component.ts
+++ src/app/shared/dumb-components/top-button/top-button.component.ts
@@ -1,20 +1,27 @@
-import { Component, HostListener, signal } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, computed, HostListener, signal } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-top-button',
-  imports: [TranslateModule, MatTooltipModule],
+  imports: [TranslateModule, MatTooltipModule, NgStyle],
   templateUrl: './top-button.component.html',
   styleUrl: './top-button.styles.scss',
 })
 export class TopIconButtonComponent {
+  readonly isTouchScreen = matchMedia('(hover: none)').matches;
+
   offsetTop = signal(0);
 
   @HostListener('window:scroll', ['$event'])
   onScroll() {
     this.offsetTop.set(document.documentElement.scrollTop);
   }
+
+  isButtonVisible = computed(() => {
+    return this.offsetTop() >= 500;
+  });
 
   scrollTop() {
     document.body.scrollIntoView({
