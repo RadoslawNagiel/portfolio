475ff1f52ceb9367e73be6b76e895196b2eb43d1
RadoslawNagiel <nagiel.media@gmail.com>
2025-09-03 17:01:35 +0200

    Fix: translations

--- src/app/shared/pipes/tooltip-value.pipe.ts
+++ src/app/shared/pipes/tooltip-value.pipe.ts
@@ -1,20 +1,48 @@
-import { inject, Pipe, PipeTransform } from '@angular/core';
-import { isTouchScreen } from '../functions/is-touch-screen';
+import {
+  Pipe,
+  PipeTransform,
+  ChangeDetectorRef,
+  OnDestroy,
+} from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Pipe({
   name: 'tooltipValue',
+  pure: false,
 })
-export class TooltipValuePipe implements PipeTransform {
-  readonly translateService = inject(TranslateService);
+export class TooltipValuePipe implements PipeTransform, OnDestroy {
+  private lastValue: string | undefined;
+  private translated: string | null = null;
+  private subscription?: Subscription;
+
+  constructor(
+    private translate: TranslateService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   transform(value?: string): string | null {
-    if (isTouchScreen()) {
-      return null;
-    }
     if (!value) {
       return null;
     }
-    return this.translateService.instant(value);
+
+    if (value === this.lastValue && this.translated !== null) {
+      return this.translated;
+    }
+
+    this.lastValue = value;
+
+    this.subscription?.unsubscribe();
+
+    this.subscription = this.translate.get(value).subscribe((translated) => {
+      this.translated = translated;
+      this.cdr.markForCheck();
+    });
+
+    return this.translated;
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
   }
 }
