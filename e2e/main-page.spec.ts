bd96f97fe4571487278dc2adb9ad2808e3b85da0
RadoslawNagiel <nagiel.media@gmail.com>
2025-09-03 18:12:57 +0200

    Chore: configure angular e2e tests

--- e2e/main-page.spec.ts
+++ e2e/main-page.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@playwright/test';
 
 test.describe(`Main page`, () => {
-  test(`should have header text`, async ({ page }) => {
-    await page.goto(`http://localhost:4200/`);
+  test('should have header text', async ({ page }) => {
+    await page.goto('/');
     await expect(page.getByRole(`banner`)).toContainText(
       `Portfolio – Radosław Nagiel`
     );
